f1c73ff1e3e8fb6146d6dcaa7eca5a8ffc334ce4
AndresCastilloPlata <plataandres26@gmail.com>
2025-05-04 16:20:36 -0500

    add Task entity in domain layer

---
+++ lib/features/tasks/domain/entities/task.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+
+class Task extends Equatable {
+  final String id;
+  final String title;
+  final bool isDone;
+
+  const Task({required this.id, required this.title, this.isDone = false});
+
+  Task copyWith({String? id, String? title, bool? isDone}) {
+    return Task(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, title, isDone];
+}
